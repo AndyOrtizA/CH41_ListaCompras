569f3322483fe5833052e05c1e84e6e38dfef01b
Andrea <andreaoal98@gmail.com>
2024-05-28 13:00:22 -0600

    se agregaron las validaciones de los campos

--- js/main.js
+++ js/main.js
@@ -1,1 +1,46 @@
 // El código va aquí 
+let btnAgregar =document.getElementById("btnAgregar");
+let btnClear = document.getElementById("btnClear");
+
+let txtNombre = document.getElementById("Name");
+let txtNumber = document.getElementById("Number");
+
+let alertValidaciones = document.getElementById("alertValidaciones");
+let alertValidacionesTexto = document.getElementById("alertValidacionesTexto");
+
+function validarCantidad(){
+    if(txtNumber.value.length==0){
+        return false;
+    }//if length
+    return true;
+}//validarCantidad
+
+btnAgregar.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    alertValidacionesTexto.innerHTML="";
+    alertValidaciones.style.display="none";
+    txtNombre.style.border="";
+    txtNumber.style.border="";
+
+
+    if (txtNombre.value.length<3){
+        alertValidacionesTexto.innerHTML="El <strong>nombre</strong> no es correcto <br/>";
+        alertValidaciones.style.display="block";
+        txtNombre.style.border="solid red medium";
+    }//length
+    if(! validarCantidad()){
+        alertValidacionesTexto.innerHTML +="El <strong>numero</strong> no es correcto";
+        alertValidaciones.style.display="block";
+        txtNumber.style.border="solid red medium";
+    }//!ValidarCantidad
+    
+});
+
+btnClear.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    txtNombre.value = "";
+    txtNumber.value = "";
+    
+});
